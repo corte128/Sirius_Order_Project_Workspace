c0236ffa6327ec77c0519a81e6b36010baef5b7e
bfillerSirius <bradenfiller@yahoo.com>
2017-09-13 16:07:42 -0500

    alrogithm for search

---
+++ ActualvBudgetSearchWS/src/com/sirius/search/beans/ActualvBudgetBean.java
@@ -0,0 +1,9 @@
+package com.sirius.search.beans;
+
+import java.math.BigDecimal;
+
+public class ActualvBudgetBean {
+
+	private BigDecimal actual;
+	private BigDecimal budget;
+}
---
+++ ActualvBudgetSearchWS/src/com/sirius/search/beans/BudgetObject.java
@@ -0,0 +1,37 @@
+package com.sirius.search.beans;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class BudgetObject {
+
+	private int id;
+	private BigDecimal actual;
+	private BigDecimal budget;
+	private Date date;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public BigDecimal getActual() {
+		return actual;
+	}
+	public void setActual(BigDecimal actual) {
+		this.actual = actual;
+	}
+	public BigDecimal getBudget() {
+		return budget;
+	}
+	public void setBudget(BigDecimal budget) {
+		this.budget = budget;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
