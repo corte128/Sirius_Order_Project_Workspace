78829142664690572154ace767f9ca56699e29ae
Scout <scout.martinelli@siriuscom.com>
2017-09-15 14:37:38 -0500

    added the rest of the cart service

--- DB SQL Scripts/locationBudgetOrderView.sql
+++ DB SQL Scripts/locationBudgetOrderView.sql
@@ -1,5 +1,5 @@
 CREATE VIEW location_budget_order_view AS
-    SELECT 
+    SELECT
         loc.location_id_PK,
         ord.order_id_pk,
         bud.budget_id_pk,
@@ -9,12 +9,14 @@ CREATE VIEW location_budget_order_view AS
         ord.product_id_fk,
         ord.total_price,
         ord.quantity,
-        ord.is_cart
-    FROM budget_tbl AS bud
-		JOIN location_tbl AS loc
-			ON bud.location_id_fk = loc.location_id_pk
-		JOIN order_tbl AS ord
-			ON bud.order_id_fk = ord.order_id_pk;
+        ord.is_cart,
+        ord.is_valid
+    FROM
+        budget_tbl AS bud
+            JOIN
+        location_tbl AS loc ON bud.location_id_fk = loc.location_id_pk
+            JOIN
+        order_tbl AS ord ON bud.order_id_fk = ord.order_id_pk;
             
 
 		
