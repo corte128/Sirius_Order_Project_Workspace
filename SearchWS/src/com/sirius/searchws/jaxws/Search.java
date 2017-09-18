a600734c3c8e5be38d94ff7fd873dea10640de87
bfillerSirius <bradenfiller@yahoo.com>
2017-09-18 10:17:03 -0500

    change to search

--- SearchWS/src/com/sirius/searchws/jaxws/Search.java
+++ SearchWS/src/com/sirius/searchws/jaxws/Search.java
@@ -19,7 +19,7 @@ public interface Search {
 	@WebResult(name = "budgetSearchReturn", targetNamespace = "http://searchws.sirius.com/search/wsdl")
 	@RequestWrapper(localName = "budgetSearch", targetNamespace = "http://searchws.sirius.com/search/wsdl", className = "com.sirius.searchws.jaxws.BudgetSearch")
 	@ResponseWrapper(localName = "budgetSearchResponse", targetNamespace = "http://searchws.sirius.com/search/wsdl", className = "com.sirius.searchws.jaxws.BudgetSearchResponse")
-	public List<ActualvBudgetBean> search(
+	public List<ActualvBudgetBean> budgetSearch(
 			@WebParam(name = "location", targetNamespace = "http://searchws.sirius.com/search/wsdl") int location_id,
 			@WebParam(name = "from", targetNamespace = "http://searchws.sirius.com/search/wsdl") Date fromDate,
 			@WebParam(name = "to", targetNamespace = "http://searchws.sirius.com/search/wsdl") Date toDate,
