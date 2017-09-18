daf55e73a257990302d451a4a48f940ebae414a8
corte128 <wjc9578@gmail.com>
2017-09-18 11:03:58 -0500

    fixed merge conflict on web.xml

--- SiriusOrderClient/src/com/sirius/searchws/search/wsdl/SearchClientDAO.java
+++ SiriusOrderClient/src/com/sirius/searchws/search/wsdl/SearchClientDAO.java
@@ -1,7 +1,12 @@
 package com.sirius.searchws.search.wsdl;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 
 public class SearchClientDAO {
 	public static List<ActualvBudgetBean> budgetSearch(int locationId, 
@@ -9,8 +14,26 @@ public class SearchClientDAO {
 			Date toDate,
 			String reportType)
 	{
+		GregorianCalendar f = new GregorianCalendar();
+		GregorianCalendar t = new GregorianCalendar();
+		
+		f.setTime(fromDate);
+		t.setTime(toDate);
+		
+		XMLGregorianCalendar gFromDate = null;
+		XMLGregorianCalendar gToDate = null;
+		try
+		{
+			gFromDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(f);
+			gToDate = DatatypeFactory.newInstance().newXMLGregorianCalendar(t);
+		} 
+		catch (DatatypeConfigurationException e) 
+		{
+			e.printStackTrace();
+		}
+		
 		SearchProxy client = new SearchProxy();
-		return client.budgetSearch(locationId, fromDate, toDate, reportType);
+		return client.budgetSearch(locationId, gFromDate, gToDate, reportType);
 		
 	}
 }
