0907090a4b1fc80af45f976cd4187f8933f6ea4b
Scout <scout.martinelli@siriuscom.com>
2017-09-19 15:58:53 -0500

    worked on mobile design of welcome page

--- SiriusOrderClient/WebContent/js/superAdminTable.js
+++ SiriusOrderClient/WebContent/js/superAdminTable.js
@@ -1,54 +1,47 @@
-var superAdminTable = angular.module('superAdminTable', [ 'ngTouch',
-		'ngAnimate', 'ui.bootstrap' ]);
+var app = angular.module('superAdminTable', [ 'ngTouch', 'ui.grid' ]);
 
-superAdminTable
-		.controller(
-				'SuperAdminCtrl',
-				[
-						'$scope',
-						'$http',
-						'$q',
-						function($scope, $http, $q) {
+app.controller('superAdminCtrl', [
+		'$scope',
+		'$http',
+		'$q',
+		function($scope, $http, $q) {
+			$scope.gridOptions = {
+					data : [],
+					columnDefs : [
+							{
+								name : 'Location',
+								width : 100,
+								pinnedLeft : true
+							},
+							{
+								name : 'Admin Name',
+								width : 100
+							},
+							{
+								name : 'Admin Email',
+								width : 150
+							},
+							{
+								name : 'No of Employees',
+								width : 100
+							},
+							{
+								name : 'Recommended Budget',
+								width : 100,
+								cellTemplate : '<div class="ui-grid-cell-contents"><input type="text" class="super-admin-input" value=\'{{row.entity["Recommended Budget"]}}\'/></div>'
+							},
+							{
+								name : 'Allotted Budget',
+								width : 100,
+								cellTemplate : '<div class="ui-grid-cell-contents"><input type="text" class="super-admin-input" value=\'{{row.entity["Allotted Budget"]}}\'/></div>'
+							} ]
+				};
 
-							$scope.gridOptions = {
-								data : [],
-								columnDefs : [
-										{
-											name : 'Location',
-											width : 100,
-											pinnedLeft : true
-										},
-										{
-											name : 'Admin Name',
-											width : 100
-										},
-										{
-											name : 'Admin Email',
-											width : 150
-										},
-										{
-											name : 'No. Of Employees',
-											width : 100
-										},
-										{
-											name : 'Recommended Budget',
-											width : 100,
-											cellTemplate : '<div class="ui-grid-cell-contents"><input type="text" class="super-admin-input">{{row.entity.recomendedBudget}}</input></div>'
-										},
-										{
-											name : 'Allotted Budget',
-											width : 100,
-											cellTemplate : '<div class="ui-grid-cell-contents"><input type="text" class="super-admin-input">{{row.entity.allottedBudget}}</input></div>'
-										} ]
-							};
+				$http.get(
+						"/SiriusOrderClient/SuperAdminServlet")
+						.then(function(response) {
+							$scope.gridOptions.data = response.data;
+							console.log($scope.gridOptions.data);
+						});
 
-							$http
-									.get("/SiriusOrderClient/SuperAdminServlet")
-									.then(
-											function(response) {
-
-												console.log(response.data);
-												$scope.gridOptions.data = response.data;
-											});
-
-						} ]);
+		} ]);
