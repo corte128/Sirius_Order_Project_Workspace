b3fc940e9e6a538d6d217f078a7868dc665b23af
harlowc <Christopher.Harlow@siriuscom.com>
2017-09-19 08:49:56 -0500

    pushing  attendance report view

--- SiriusOrderClient/WebContent/js/attendanceTable.js
+++ SiriusOrderClient/WebContent/js/attendanceTable.js
@@ -31,14 +31,33 @@ app.controller(
 											width : 130
 										} ]
 							};
+							function getSearch(){
+								
+								var firstName = document.getElementById("firstName").value;
+								var lastName = document.getElementById("lastName").value;
+								var email = document.getElementById("email").value;
+								var role = document.getElementById("role").value;
+								if(firstName==''){
+									firstName = "%";
+								}
+								if(lastName == ''){
+									lastName="%";
+								}
+								if(email == ''){
+									email="%";
+								}
 
 							$http
-									.get("/SiriusOrderClient/AttendanceServlet")
+									.get("/SiriusOrderClient/AttendanceServlet?query=getRecords?name="+name+"?email="+email+"?startDate="+startDate+"?endDate="+endDate+
+											"?location="+location+"?range="+range)
 									.then(
 											function(response) {
 
 												console.log(response.data);
 												$scope.gridOptions.data = response.data;
 											});
+							}
+							$scope.getSearch =getSearch;
+							
 
 }]);
