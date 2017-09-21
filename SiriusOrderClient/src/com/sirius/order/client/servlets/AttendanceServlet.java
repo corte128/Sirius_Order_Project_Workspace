ef33030cf59ef92584dffd0c6b86d6ae60485399
harlowc <Christopher.Harlow@siriuscom.com>
2017-09-21 16:12:58 -0500

    adding pdf functionality

--- SiriusOrderClient/src/com/sirius/order/client/servlets/AttendanceServlet.java
+++ SiriusOrderClient/src/com/sirius/order/client/servlets/AttendanceServlet.java
@@ -1,5 +1,6 @@
 package com.sirius.order.client.servlets;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.DateFormat;
@@ -13,6 +14,7 @@ import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonWriter;
+import javax.print.Doc;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +22,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
+import org.apache.pdfbox.pdmodel.font.PDType1CFont;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.PDType3Font;
 
 import be.quodlibet.boxable.BaseTable;
 import be.quodlibet.boxable.datatable.DataTable;
@@ -128,19 +135,19 @@ public class AttendanceServlet extends HttpServlet {
 				endDate = "3000-12-31";
 			}
 
-			List<AttendanceRecordBean> attendanceRecords = attendanceService
+			ArrayList<AttendanceRecordBean> attendanceRecords = attendanceService
 					.getAttendanceRecords(name, email, location, startDate,
 							endDate);
 
-			List<List> dataList = new ArrayList<List>();
-			dataList.add(new ArrayList<String>(Arrays.asList("Name", "Email",
-					"Date", "Location")));
-			for (AttendanceRecordBean record : attendanceRecords) {
-				dataList.add(new ArrayList<String>(Arrays.asList(
-						record.getAttendantName(), record.getAttendantEmail(),
-						record.getAttendantDate(),
-						record.getAttendantLocation())));
-			}
+//			List<List> dataList = new ArrayList<List>();
+//			dataList.add(new ArrayList<String>(Arrays.asList("Name", "Email",
+//					"Date", "Location")));
+//			for (AttendanceRecordBean record : attendanceRecords) {
+//				dataList.add(new ArrayList<String>(Arrays.asList(
+//						record.getAttendantName(), record.getAttendantEmail(),
+//						record.getAttendantDate(),
+//						record.getAttendantLocation())));
+//			}
 
 			if (view.equalsIgnoreCase("display")) {
 				JsonArrayBuilder builder = Json.createArrayBuilder();
@@ -157,37 +164,97 @@ public class AttendanceServlet extends HttpServlet {
 				JsonWriter writer = Json.createWriter(out);
 				writer.writeArray(output);
 				writer.close();
-
 			} else if (view.equalsIgnoreCase("pdf")) {
-				PDPage myPage = new PDPage(PDRectangle.A4);
+				PDPage myPage = new PDPage();
 				PDDocument mainDocument = new PDDocument();
+				
+				mainDocument.addPage(myPage);
+				String path = "C:/pdfTestFiles/test-PDF.pdf";
+				PDPageContentStream contentStream = new PDPageContentStream(mainDocument, myPage);
+				
+				//set a count to keep track of the pages we need to create
+				int count =0;
+				//start the creation of the contentStream
+				contentStream.beginText();
+				
+				//set the font
+				contentStream.setFont(PDType1Font.HELVETICA, 16);
+				
+				
+				//set the leading
+				contentStream.setLeading(14.5f);
+				
+				//set the position for the first line
+				contentStream.newLineAtOffset(25, 725);
+				//format our strings so we get a nice output
+				String formatString = "%-40s%-40s%-40s%-40s";
+				
+				// this will be our header
+				String headerTextString = String.format(formatString, "Name", "Email", "Date", "Location");
+				
+				//add the content of header to the document
+				contentStream.showText(headerTextString);
+				contentStream.newLine();
+				// now we set the text smaller for the actual content
+				contentStream.setFont(PDType1Font.HELVETICA, 14);
+				//next we get the content and output it line by line
+				
+				
+				for(AttendanceRecordBean record: attendanceRecords){
+					String contentToAdd = String.format(formatString, record.getAttendantName(), record.getAttendantEmail(), record.getAttendantDate(), record.getAttendantLocation());
+					contentStream.showText(contentToAdd);
+					contentStream.newLine();
+					count++;
+					if (count==47){
+						contentStream.endText();
+						myPage = new PDPage();
+						mainDocument.addPage(myPage);
+						contentStream = new PDPageContentStream(mainDocument, myPage);
+						contentStream.beginText();
+						count =0;
+					}
+				}
+				
+				//end the text
+				contentStream.endText();
+				
+				//close our contentStream
+				contentStream.close();
+				
+				
+				//save the document
+				mainDocument.save(new File(path));
+				
+				//close the document
+				
+				mainDocument.close();
+//				// Dummy Table
+//				float margin = 50;
+//				// starting y position is whole page height subtracted by top
+//				// and bottom margin
+//				float yStartNewPage = myPage.getMediaBox().getHeight()
+//						- (2 * margin);
+//				// we want table across whole page width (subtracted by left and
+//				// right margin ofcourse)
+//				float tableWidth = myPage.getMediaBox().getWidth()
+//						- (2 * margin);
+//
+//				boolean drawContent = true;
+//				float yStart = yStartNewPage;
+//				float bottomMargin = 70;
+//				// y position is your coordinate of top left corner of the table
+//				float yPosition = 550;
+//
+//				BaseTable dataTable = new BaseTable(yStart, yStartNewPage,
+//						bottomMargin, tableWidth, margin, mainDocument, myPage,
+//						true, drawContent);
+//				DataTable t = new DataTable(dataTable, myPage);		//TODO: crashes here
+//				t.addListToTable(dataList, DataTable.HASHEADER);
+//				dataTable.draw();
+//				mainDocument.addPage(myPage);
+//				mainDocument.save("C:/pdfTestFiles/testPDF.pdf");
+//				mainDocument.close();
 
-				// Dummy Table
-				float margin = 50;
-				// starting y position is whole page height subtracted by top
-				// and bottom margin
-				float yStartNewPage = myPage.getMediaBox().getHeight()
-						- (2 * margin);
-				// we want table across whole page width (subtracted by left and
-				// right margin ofcourse)
-				float tableWidth = myPage.getMediaBox().getWidth()
-						- (2 * margin);
-
-				boolean drawContent = true;
-				float yStart = yStartNewPage;
-				float bottomMargin = 70;
-				// y position is your coordinate of top left corner of the table
-				float yPosition = 550;
-
-				BaseTable dataTable = new BaseTable(yStart, yStartNewPage,
-						bottomMargin, tableWidth, margin, mainDocument, myPage,
-						true, true);
-				DataTable t = new DataTable(dataTable, myPage);
-				t.addListToTable(dataList, DataTable.HASHEADER);
-				dataTable.draw();
-			    mainDocument.addPage(myPage);
-			    mainDocument.save("C://pdfTestFiles/testfile.pdf");
-			    mainDocument.close();
 			}
 		}
 
