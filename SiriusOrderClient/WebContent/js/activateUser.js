d8be6706b8b7dc74dc29782fe965bb25429010fa
Cherie Parsons <cap377@nau.edu>
2017-09-22 11:52:24 -0500

    still working on responsive design

--- SiriusOrderClient/WebContent/js/activateUser.js
+++ SiriusOrderClient/WebContent/js/activateUser.js
@@ -1,11 +1,9 @@
 function approve(btn){
-	console.log("approving...");
 	$('#pressed').val("approved");
 	$('#id').val(btn.value);
 	alert("Employee Approved!");
 };
 function reject(btn){
-	console.log("rejecting...");
 	$('#pressed').val("rejected");
 	$('#id').val(btn.value);
 	alert("Employee Rejected!");
