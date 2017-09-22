e37bb1557f06050374c4d99029078c9ce3c8ac9e
bfillerSirius <bradenfiller@yahoo.com>
2017-09-22 12:40:39 -0500

    fix adding to wishlist

--- SiriusOrderClient/WebContent/js/productSearch.js
+++ SiriusOrderClient/WebContent/js/productSearch.js
@@ -44,7 +44,7 @@ function searchProducts()
 	var search = document.getElementById("search");
 	var category = document.getElementById("category");
 	
-	var url = "/SiriusOrderClient/ProductSearchServlet?query=searchProducts&category=" + category.value + "&search=" +  search.value;
+	var url = "/SiriusOrderClient/ProductSearchServlet?action=searchProducts&category=" + category.value + "&search=" +  search.value;
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("GET", url, true);
 	xhttp.onreadystatechange = function()
@@ -88,7 +88,7 @@ function searchProducts()
 			
 			var spanHeart = document.createElement("span");
 			spanHeart.setAttribute("class", "glyphicon glyphicon-heart clickable-like");
-			spanHeart.onclick=addToWishlist(response[key].ID);
+			spanHeart.onclick='addToWishlist(' + response[key].id + ')';
 			/*==============================*/
 			
 			var spanPrice = document.createElement("span");
